4d5284ffbc033fd2099474f4536401ed7603787b
markgreenburg <marek.greenburg@gmail.com>
2017-02-15 22:37:42 -0600

    Adding demo coin flip page

---
+++ reactdemo/src/js/client.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Layout from "./components/Components";
+
+const App = document.getElementById("container");
+
+ReactDOM.render(<Layout />, App);
---
+++ reactdemo/src/js/components/Components.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class Header extends React.Component{
+	render(){
+		return <div>I am the HEADER</div>;
+	}
+}
+
+class Footer extends React.Component {
+	render() {
+		return <div>I am the FOOTER</div>;
+	}
+}
+
+class Coin extends React.Component {
+	constructor() {
+		super();
+		this.sides = 			['http://www.marshu.com/articles/images-website/articles/presidents-on-coins/half-dollar-coin-tail.jpg','http://www.marshu.com/articles/images-website/articles/presidents-on-coins/quarter-coin-head.jpg'
+		]
+		this.state = {
+			image: this.sides[Math.round(Math.random());],
+			flipped: false
+		}
+	}
+
+	const flip = () => {
+		const side = Math.round(Math.random());
+		this.setState({image: this.sides[side]});
+	};
+
+}
+
+export default class Layout extends React.Component{
+	constructor(){
+		super()
+		this.state = {
+			number:3
+		};
+	}
+
+	render(){
+		return(
+			<div>
+				<Header />
+				<div>I am JSX</div>
+				<Coin />
+				<Footer />
+			</div>
+		);
+	}
+}
