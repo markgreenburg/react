e711fef59186dd2a10c5fde3dd0cb930ced3d22c
markgreenburg <marek.greenburg@gmail.com>
2017-03-03 23:01:52 -0600

    refactored fetch() code

--- webpackreact/js/components/List.js
+++ webpackreact/js/components/List.js
@@ -3,29 +3,14 @@ import React from "react";
 export default class List extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            goog: 0,
-            aapl: 0,
-            fb: 0
-        };
+        this.state = { goog: 0, };
     }
 
     componentDidMount() {
         fetch("http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=goog")
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    console.log("There was a problem getting stock price. Status Code: " + response.status);
-                }
-            })
-            .then((data) => {
-                console.log(data);
-                this.setState({goog: data.LastPrice});
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+            .then((response) => response.json())
+            .then((data) => this.setState({goog: data.LastPrice}))
+            .catch((err) => console.log(err));
     }
 
     render() {
